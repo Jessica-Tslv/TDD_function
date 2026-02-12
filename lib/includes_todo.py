eaac0f786471530fea260cf76239d4480a34918f
jessica.tsolova <jessica.tsolova@moonpig.com>
2026-02-12 17:17:56 +0000

    Solved the rest of the tests - thanks Jay :)

--- lib/includes_todo.py
+++ lib/includes_todo.py
@@ -1,7 +1,7 @@
 
 def includes_todo(text):
     # it should check #TODO
-    if "#TODO" in text:
+    if "#TODO" in text or "#todo" in text:
         return True
     else:    
         return False
--- tests/test_includes_todo.py
+++ tests/test_includes_todo.py
@@ -25,3 +25,29 @@ Expected output should be True
 def test_returns_true_with_todo_at_the_end():
     line_example = includes_todo("Buy groceries #TODO")
     assert line_example == True
+
+
+"""
+Test with a line containing #TODO with punctuation - e.g. #TODO:
+Expected output should be True
+"""
+def test_returns_true_with_punctuation():
+    line_example = includes_todo("#TODO: Buy groceries")
+    assert line_example == True
+
+"""
+Test with lower case #todo
+Expected output should be True
+"""
+def test_returns_true_with_lowercase_todo():
+    line_example = includes_todo("#todo: Buy groceries")
+    assert line_example == True
+
+"""
+Test without the hash symbol - e.g. TODO
+Expected output should be False
+"""
+
+def test_returns_false_without_hash_symbol():
+    line_example = includes_todo("TODO Buy groceries")
+    assert line_example == False
